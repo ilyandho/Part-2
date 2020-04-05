698d62ca7052bbf72454a94f14b017e8d23cfbf9
Ndhote Ilyas <ii.ndhote@gmail.com>
2020-04-05 23:25:58 +0300

    Exercises 2.15.-2.18.

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
 import Filter from "./components/Filter";
-import Persons from "./components/Person";
+import Persons from "./components/Persons";
 import NewNumbers from "./components/NewNumbers";
+
+import personService from "./services/persons";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -17,27 +18,65 @@ const App = () => {
   const handleNumberChange = (e) => {
     setNewNumber(e.target.value);
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const contact = { name: newName, number: newNumber };
     let exists = false;
+    let existingUserId;
     for (let i = 0; i < persons.length; i++) {
       //Check if the contact name is not alreayd added
       if (persons[i].name.toLowerCase() === newName.toLowerCase()) {
         exists = true;
+        existingUserId = persons[i].id;
       }
     }
 
     exists
-      ? alert(`${newName} is already added to phonebook`)
-      : setPersons(persons.concat(contact));
+      ? window.confirm(
+          `${newName} is already added to phonebook, replace the old one with a new one?`
+        )
+        ? updatePerson(existingUserId, contact)
+        : console.log("Not added")
+      : personService
+          .create(contact)
+          .then((returnedPersons) =>
+            setPersons(persons.concat(returnedPersons))
+          );
   };
 
-  const personsHook = () => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data);
+  const handleDelete = (id, name) => {
+    console.log("id", id);
+    const confirmation = window.confirm(`Delete ${name}`);
+    console.log(confirmation);
+
+    confirmation
+      ? personService.remove(id).then((response) => {
+          console.log("deleted", response);
+          const afterDelete = persons.filter(
+            (person) => person.id !== response.id
+          );
+          console.log("after", afterDelete);
+          setPersons(afterDelete);
+        })
+      : console.log("not deleted");
+  };
+
+  const updatePerson = (id, userObject) => {
+    personService.update(id, userObject).then((updateInfo) => {
+      console.log(updateInfo);
+      // setPersons(updateInfo);
+      setPersons(
+        persons.map((person) => (person.id !== id ? person : updateInfo))
+      );
+      console.log(persons);
     });
+  };
+  const personsHook = () => {
+    personService
+      .getAll()
+      .then((innititalPersons) => setPersons(innititalPersons));
   };
   useEffect(personsHook, []);
 
@@ -54,7 +93,11 @@ const App = () => {
         newNumber={newNumber}
       />
       <h2>Numbers</h2>
-      <Persons persons={persons} filters={filters} />
+      <Persons
+        persons={persons}
+        filters={filters}
+        handleDelete={handleDelete}
+      />
     </div>
   );
 };
---
+++ phonebook/src/components/Persons.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Person = ({ persons, filters, handleDelete }) => {
+  return (
+    <div>
+      {persons
+        .filter((person) => person.name.toLowerCase().includes(filters))
+        .map((person) => (
+          <p key={person.name}>
+            {person.name} {person.number}{" "}
+            <button
+              type="button"
+              onClick={() => handleDelete(person.id, person.name)}
+            >
+              delete
+            </button>
+          </p>
+        ))}
+    </div>
+  );
+};
+
+export default Person;
